c5e3af793ecd6a4d846cd8c5e12f5c9b37ce6495
senzowayne <actu-senzo@hotmail.fr>
2020-04-24 20:53:00 +0200

    add max +3M and add icon next & previous

--- assets/js/components/DatePicker.vue
+++ assets/js/components/DatePicker.vue
@@ -3,7 +3,15 @@
         <div class="mt-3 pl-2 col-4 col-sm offset-1">
             <v-app id="inspire" style="height: 360px!important;">
                 <v-row align="start">
-                    <v-date-picker :min="today" dark locale="fr" full-width :landscape="$vuetify.breakpoint.smAndUp" class="mt-4"
+                    <v-date-picker :min="min" :max="max"
+                                   dark
+                                   full-width
+                                   locale="fr"
+                                   :landscape="$vuetify.breakpoint.smAndUp"
+                                   class="mt-4"
+                                   year-icon="mdi-calendar-blank"
+                                   prev-icon="mdi-skip-previous"
+                                   next-icon="mdi-skip-next"
                                    v-model="picker"></v-date-picker>
                     <input id="dateSelected" type="hidden" :value="picker">
                 </v-row>
@@ -33,8 +41,12 @@
         data() {
             return {
                 picker: new Date().toISOString().substr(0, 10),
-                today: new Date().toISOString().substr(0, 10)
+                min: new Date().toISOString().substr(0, 10),
+                max: (new Date(new Date().setMonth(new Date().getMonth() + 1))).toISOString().substr(0, 10)
             }
         }
     }
 </script>
+<style>
+    @import "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css";
+</style>
