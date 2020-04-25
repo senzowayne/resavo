98ed26a4836dc1f6615906278444d3f5934914bd
senzo <31672896+senzowayne@users.noreply.github.com>
2020-04-25 13:13:26 +0200

    ISSUE-#53 : Désactiver les dates bloquées depuis l'admin (#54)

--- assets/js/components/DatePicker.vue
+++ assets/js/components/DatePicker.vue
@@ -1,15 +1,24 @@
 <template>
     <div id="resa" class="row mt-3">
-        <div class="mt-3 pl-2 col-4 col-sm offset-1">
+        <div class="mt-3 pl-2 col-4 col-sm offset-1"> <!-- start col -->
             <v-app id="inspire" style="height: 360px!important;">
                 <v-row align="start">
-                    <v-date-picker dark locale="fr" full-width :landscape="$vuetify.breakpoint.smAndUp" class="mt-4"
-                                   v-model="picker"></v-date-picker>
-                    <input id="dateSelected" type="hidden" :value="picker">
+                    <v-date-picker :min="min" :max="max"
+                                   dark
+                                   full-width
+                                   locale="fr"
+                                   :landscape="$vuetify.breakpoint.smAndUp"
+                                   class="mt-4"
+                                   year-icon="mdi-calendar-blank"
+                                   prev-icon="mdi-skip-previous"
+                                   next-icon="mdi-skip-next"
+                                   v-model="picker"
+                                   :allowed-dates="allowedDates">
+                    </v-date-picker>
                 </v-row>
             </v-app>
         </div> <!-- end col -->
-        <div class="col">  
+        <div class="col">
             <Room v-bind:date="picker"/>
         </div>
     </div>
@@ -24,15 +33,37 @@
         components: {Room, Meeting},
         created() {
             store.commit('CHANGE_DATE', this.picker)
+            this.getDisableDate();
         },
-        watch:{
-          picker: function (newVal) {
-              store.commit('CHANGE_DATE', newVal)
-          }
+        watch: {
+            picker: function (newVal) {
+                store.commit('CHANGE_DATE', newVal)
+            }
+        },
+        methods: {
+            allowedDates(date) {
+                // Ne pas tomber sur currentDate si celle-ci est bloquée
+                if (this.disableDate.includes(this.picker) && (this.min < date && this.max > date)) {
+                    this.picker = date
+                }
+                return !this.disableDate.includes(date);
+            },
+            getDisableDate() {
+                axios.get(`/api/date_blockeds`)
+                    .then(({data}) => {
+                        let response = data['hydra:member'];
+                        this.disableDate = response.map(function (date) {
+                            return date.blockedDate.substr(0, 10);
+                        })
+                    })
+            }
         },
         data() {
             return {
-                picker: new Date().toISOString().substr(0, 10)
+                picker: new Date().toISOString().substr(0, 10),
+                min: new Date().toISOString().substr(0, 10),
+                max: (new Date(new Date().setMonth(new Date().getMonth() + 3))).toISOString().substr(0, 10),
+                disableDate: []
             }
         }
     }
