ec9649056969764ad55679ef79d3858b9e2d91b7
senzowayne <actu-senzo@hotmail.fr>
2021-05-03 14:34:57 +0200

    remove mercure + link admin

--- config/bundles.php
+++ config/bundles.php
@@ -17,6 +17,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
