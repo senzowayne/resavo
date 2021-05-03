286f3ad0a21b1b8912292c9dd6751de84a42f25b
moisebi <77336856+moisebi@users.noreply.github.com>
2021-05-03 14:30:05 +0200

    Migration easyadmin3 (#111)

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Booking;
+use App\Entity\DateBlocked;
+use App\Entity\Meeting;
+use App\Entity\Paypal;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Admin Réservation');
+    }
+
+    public function configureCrud(): Crud
+    {
+        return Crud::new()
+            ->setDateFormat('dd/MM/yyyy');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+
+        yield MenuItem::section('Réservation', 'fas fa-folder-open');
+        yield MenuItem::linkToCrud('Nouvelle Réservation', 'fas fa-plus', Booking::class);
+        yield MenuItem::linkToCrud('Réservation', 'fas fa-calendar-alt', Booking::class)->setDefaultSort(['bookingDate' => 'DESC']);
+        yield MenuItem::linktoRoute('Réservation du jour', 'fas fa-folder-open', 'resa_day')->setLinkTarget('_blank')->setLinkRel('resa-day');
+
+        yield MenuItem::section('Autres', 'fas fa-folder-open');
+        yield MenuItem::linkToCrud('Client', 'fas fa-user', User::class)->setDefaultSort(['name' => 'ASC']);
+        yield MenuItem::linkToCrud('Séance', 'fas fa-clock', Meeting::class)->setDefaultSort(['id' => 'ASC']);
+        yield MenuItem::linkToCrud('Bloqué une date', 'fas fa-clock', DateBlocked::class)->setDefaultSort(['start' => 'DESC']);
+        yield MenuItem::linkToCrud('Paiement', 'fas fa-folder-open', Paypal::class);
+    }
+}
