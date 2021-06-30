738115452330c9a0904ba5fef55ce189ee6fa137
senzo <31672896+senzowayne@users.noreply.github.com>
2021-06-30 18:58:08 +0200

    Add caddy server (#149)

---
+++ docker/php/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+	if [ "$APP_ENV" != 'prod' ]; then
+		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+	fi
+	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+
+	mkdir -p var/cache var/log
+
+	# The first time volumes are mounted, the project needs to be recreated
+	if [ ! -f composer.json ]; then
+		composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability=$STABILITY --prefer-dist --no-progress --no-interaction
+		jq '.extra.symfony.docker=true' tmp/composer.json >tmp/composer.tmp.json
+		rm tmp/composer.json
+		mv tmp/composer.tmp.json tmp/composer.json
+
+		cp -Rp tmp/. .
+		rm -Rf tmp/
+	elif [ "$APP_ENV" != 'prod' ]; then
+		rm -f .env.local.php
+		composer install --prefer-dist --no-progress --no-interaction
+	fi
+
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+fi
+
+exec docker-php-entrypoint "$@"
